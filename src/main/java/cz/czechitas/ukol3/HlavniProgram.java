0abe366bbdc72894759384f981db2d0475bf7439
Janett123 <masiko@centrum.cz>
2023-03-10 14:14:10 +0100

    První část - třída Disk, Pamet, Procesor, Pocitac,   - u všech toString, zapnutí a vypnutí PC, chyby:   chybí součást PC, volám víckrát zapniSe

--- src/main/java/cz/czechitas/ukol3/HlavniProgram.java
+++ src/main/java/cz/czechitas/ukol3/HlavniProgram.java
@@ -1,13 +1,53 @@
 package cz.czechitas.ukol3;
 
+import cz.czechitas.ukol3.model.Disk;
+import cz.czechitas.ukol3.model.Pamet;
+import cz.czechitas.ukol3.model.Pocitac;
+import cz.czechitas.ukol3.model.Procesor;
+
 /**
  * Spouštěcí třída celého programu
  */
 public class HlavniProgram {
 
     public static void main(String... args) {
+
+        Disk mujDisk = new Disk();
+        mujDisk.setKapacita(475_000_000_000L);
+        mujDisk.setVyuziteMisto(147_000_000_000L);
+
+        Pamet mojePamet = new Pamet();
+        mojePamet.setKapacita(8_000_000_000L);
+
+        Procesor mujProcesor = new Procesor();
+        mujProcesor.setVyrobce("Intel");
+        mujProcesor.setRychlost(2_400_000_000L);
+
+        Pocitac mujPocitac = new Pocitac();
+        mujPocitac.setCpu(mujProcesor);
+        mujPocitac.setRam(mojePamet);
+        mujPocitac.setPevnyDisk(mujDisk);
+
+        mujPocitac.zapniSe();
+        mujPocitac.zapniSe();
+
+        System.out.println(mujPocitac.toString());
+
+        mujPocitac.vypniSe();
+
+        System.out.println(mujPocitac.toString());
+
+
+
+
+
+
+
+
         //TODO tady bude váš kód - vytvoření instance třídy Pocitac, zapnutí, vpynutí, výpis hodnot.
         System.out.println("Program spuštěn.");
     }
+
+
 
 }
---
+++ src/main/java/cz/czechitas/ukol3/model/Pocitac.java
@@ -0,0 +1,75 @@
+package cz.czechitas.ukol3.model;
+
+import java.util.Objects;
+
+public class Pocitac {
+
+    private boolean jeZapnuty;
+
+    private Procesor cpu;
+
+    private Pamet ram;
+
+    private Disk pevnyDisk;
+
+
+    public Procesor getCpu() {
+        return cpu;
+    }
+
+    public void setCpu(Procesor cpu) {
+        this.cpu = cpu;
+    }
+
+    public Pamet getRam() {
+        return ram;
+    }
+
+    public void setRam(Pamet ram) {
+        this.ram = ram;
+    }
+
+    public Disk getPevnyDisk() {
+        return pevnyDisk;
+    }
+
+    public void setPevnyDisk(Disk pevnyDisk) {
+        this.pevnyDisk = pevnyDisk;
+    }
+
+    @Override
+    public String toString() {
+        return "Pocitac{" +
+                "jeZapnuty=" + jeZapnuty +
+                ", cpu=" + cpu +
+                ", ram=" + ram +
+                ", pevnyDisk=" + pevnyDisk +
+                '}';
+    }
+
+
+    public void zapniSe() {
+
+        if (ram==null||cpu==null||pevnyDisk==null){
+            System.err.println("chybí součást PC");
+            jeZapnuty=false;
+            return;
+        }
+
+        if (jeZapnuty==true){
+            System.err.println("Počítač je už zapnutý");
+            return;
+        }
+        jeZapnuty=true;
+
+    }
+    public void vypniSe() {
+        jeZapnuty = false;
+    }
+    public boolean jeZapnuty() {
+        return jeZapnuty;
+    }
+
+
+
+}
